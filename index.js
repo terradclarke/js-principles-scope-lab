c75ffb2cc325a5e5ba9c8da9902dcb6695c58151
Terradclarke <terranie67@gmail.com>
2020-11-19 20:55:29 -0600

    commit tree 0e5014f27ecbd0d681eb71d46e3a5fb59d8976cf

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "the bob";
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not bob";
+}
+
+function attemptTwoFavoriteCustomers() {
+    //let favoriteCustomer = 'bar';
+    //let favoriteCustomer = 'not bar';
+    throw SyntaxError();
+}
